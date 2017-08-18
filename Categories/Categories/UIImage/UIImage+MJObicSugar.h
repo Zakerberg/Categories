cd0cd736d0e08045a33b7e6566a1878baed177e1
Zakerberg <blmichael1129@icloud.com>
2017-08-18 15:36:22 +0800

    UIImage

---
+++ Categories/Categories/UIImage/UIImage+MJObicSugar.h
@@ -0,0 +1,21 @@
+//
+//  UIImage+MJObicSugar.h
+//  Categories
+//
+//  Created by Michael 柏 on 2017/8/18.
+//  Copyright © 2017年 Michael 柏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (MJObicSugar)
+
+/// 生成指定颜色的一个`点`的图像
+///
+/// @param color 颜色
+///
+/// @return 1 * 1 图像
+
++ (nonnull UIImage *)mj_singleDotImageWithColor:(nonnull UIColor *)color;
+
+@end
---
+++ Categories/Categories/UIImage/UIImage+MJObicSugar.m
@@ -0,0 +1,26 @@
+//
+//  UIImage+MJObicSugar.m
+//  Categories
+//
+//  Created by Michael 柏 on 2017/8/18.
+//  Copyright © 2017年 Michael 柏. All rights reserved.
+//
+
+#import "UIImage+MJObicSugar.h"
+
+@implementation UIImage (MJObicSugar)
+
++ (UIImage *)mj_singleDotImageWithColor:(UIColor *)color {
+    
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(1, 1), YES, 0);
+    
+    [color setFill];
+    UIRectFill(CGRectMake(0, 0, 1, 1));
+    
+    UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return result;
+}
+
+@end
